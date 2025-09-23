a448814e50a67647ef2eb30138d86d6d8dcc0130
LucasBoeira <shaeikinho@gmail.com>
2025-09-22 22:00:47 -0300

    adicionei as tabelas e o util que tinha feito

---
+++ src/tools/Util.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+
+import java.util.Date;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author u05521390170
+ */
+public class Util {
+    public static void habilitar(boolean valor, JComponent ... componentes){
+        for (int i = 0; i < componentes.length; i++) {
+            componentes[i].setEnabled(valor);
+            
+        }
+    }
+    public static void limpar(JComponent ... componentes){
+        for (int i = 0; i < componentes.length; i++) {
+            if (componentes[i] instanceof JTextField){
+            ((JTextField) componentes[i]).setText("");
+        } 
+            if (componentes[i] instanceof JComboBox){
+            ((JComboBox) componentes[i]).setSelectedIndex(-1);
+        } 
+        if (componentes[i] instanceof JFormattedTextField){
+            ((JFormattedTextField) componentes[i]).setText("");
+        }
+        if (componentes[i] instanceof JPasswordField){
+            ((JPasswordField) componentes[i]).setText("");
+        }
+        if (componentes[i] instanceof JCheckBox){
+            ((JCheckBox) componentes[i]).setSelected(false);
+        }
+    }
+    }
+    public static void mensagem(String cad){
+        JOptionPane.showMessageDialog(null, cad);
+    
+    }
+    public static boolean perguntar(String cad){
+    JOptionPane.showConfirmDialog(null, cad);
+    return true;
+    }
+    public static int strToInt(String cad){
+        return Integer.valueOf(cad);
+    
+    }
+    public static String intToStr(int num){
+    return String.valueOf(num);
+    }
+    public static double strToDouble(String cad){
+    return Double.valueOf(cad);
+    }
+    
+    //VI VARIOS VIDEOS PROFESSOR E ESSE FOI MODO QUE EU CONSEGUI ENTENDER OQ REALMENTE ACONTECE DE FAZER O DATE
+   public static Date strToDate(String cad){
+    try {
+        return new java.text.SimpleDateFormat("dd/MM/yyyy").parse(cad);
+    } catch (Exception e) {
+        return null;
+    }
+}
+
+public static String dateToStr(Date data){
+    return new java.text.SimpleDateFormat("dd/MM/yyyy").format(data);
+    
+}
+}
---
+++ src/view/JFrmPrincipal.java
@@ -0,0 +1,233 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package view;
+
+/**
+ *
+ * @author u04893768190
+ */
+public class JFrmPrincipal extends javax.swing.JFrame {
+
+    /**
+     * Creates new form JFrmPrincipal
+     */
+    public JFrmPrincipal() {
+        initComponents();
+        setExtendedState(MAXIMIZED_BOTH);
+        setTitle("Sistema de vendas Veiculoa");
+    }
+
+   
+ 
+
+//    /**
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuBar2 = new javax.swing.JMenuBar();
+        jMnuCadastro = new javax.swing.JMenu();
+        jMnuUsuarios = new javax.swing.JMenuItem();
+        jMnuVendedor = new javax.swing.JMenuItem();
+        jMnuMecanico = new javax.swing.JMenuItem();
+        jMnuClientes = new javax.swing.JMenuItem();
+        jMnuVeiculos = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
+        jMnuSair = new javax.swing.JMenuItem();
+        jMnuMovimento = new javax.swing.JMenu();
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+        jMenuBar1.add(jMenu2);
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jMnuCadastro.setMnemonic('c');
+        jMnuCadastro.setText("Cadastros");
+        jMnuCadastro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMnuCadastroActionPerformed(evt);
+            }
+        });
+
+        jMnuUsuarios.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_U, java.awt.event.InputEvent.CTRL_MASK));
+        jMnuUsuarios.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/cliente_1.png"))); // NOI18N
+        jMnuUsuarios.setMnemonic('u');
+        jMnuUsuarios.setText("Usuarios");
+        jMnuUsuarios.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMnuUsuariosActionPerformed(evt);
+            }
+        });
+        jMnuCadastro.add(jMnuUsuarios);
+
+        jMnuVendedor.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_L, java.awt.event.InputEvent.CTRL_MASK));
+        jMnuVendedor.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/usuario.png"))); // NOI18N
+        jMnuVendedor.setText("Vendedor");
+        jMnuVendedor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMnuVendedorActionPerformed(evt);
+            }
+        });
+        jMnuCadastro.add(jMnuVendedor);
+
+        jMnuMecanico.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_K, java.awt.event.InputEvent.CTRL_MASK));
+        jMnuMecanico.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/mecanico2.png"))); // NOI18N
+        jMnuMecanico.setText("Mecanico");
+        jMnuMecanico.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMnuMecanicoActionPerformed(evt);
+            }
+        });
+        jMnuCadastro.add(jMnuMecanico);
+
+        jMnuClientes.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_J, java.awt.event.InputEvent.ALT_MASK));
+        jMnuClientes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/usuario.png"))); // NOI18N
+        jMnuClientes.setText("Clientes");
+        jMnuClientes.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMnuClientesActionPerformed(evt);
+            }
+        });
+        jMnuCadastro.add(jMnuClientes);
+
+        jMnuVeiculos.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_P, java.awt.event.InputEvent.CTRL_MASK));
+        jMnuVeiculos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/imagemCarro.png"))); // NOI18N
+        jMnuVeiculos.setMnemonic('p');
+        jMnuVeiculos.setText("Veiculos");
+        jMnuVeiculos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMnuVeiculosActionPerformed(evt);
+            }
+        });
+        jMnuCadastro.add(jMnuVeiculos);
+        jMnuCadastro.add(jSeparator1);
+
+        jMnuSair.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Z, java.awt.event.InputEvent.CTRL_MASK));
+        jMnuSair.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/exit_1.png"))); // NOI18N
+        jMnuSair.setText("Sair");
+        jMnuSair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMnuSairActionPerformed(evt);
+            }
+        });
+        jMnuCadastro.add(jMnuSair);
+
+        jMenuBar2.add(jMnuCadastro);
+
+        jMnuMovimento.setText("Movimento");
+        jMenuBar2.add(jMnuMovimento);
+
+        setJMenuBar(jMenuBar2);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 277, Short.MAX_VALUE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jMnuCadastroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMnuCadastroActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMnuCadastroActionPerformed
+
+    private void jMnuUsuariosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMnuUsuariosActionPerformed
+        // TODO add your handling code here:
+        JDlgUsuarios jDlgUsuarios = new JDlgUsuarios(null, true);
+        jDlgUsuarios.setVisible(true);
+    }//GEN-LAST:event_jMnuUsuariosActionPerformed
+
+    private void jMnuSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMnuSairActionPerformed
+System.exit(0);        // TODO add your handling code here:
+    }//GEN-LAST:event_jMnuSairActionPerformed
+
+    private void jMnuVendedorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMnuVendedorActionPerformed
+        // TODO add your handling code here:
+        JDlgVendedor jDlgVendedor = new JDlgVendedor(null, true);
+        jDlgVendedor.setVisible(true);
+    }//GEN-LAST:event_jMnuVendedorActionPerformed
+
+    private void jMnuMecanicoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMnuMecanicoActionPerformed
+       JDlgMecanico jDlgMecanico = new JDlgMecanico(null, true);
+        jDlgMecanico.setVisible(true); // TODO add your handling code here:
+    }//GEN-LAST:event_jMnuMecanicoActionPerformed
+
+    private void jMnuClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMnuClientesActionPerformed
+       JDlgClientes jDlgClientes = new JDlgClientes(null, true);
+        jDlgClientes.setVisible(true); // TODO add your handling code here:
+    }//GEN-LAST:event_jMnuClientesActionPerformed
+
+    private void jMnuVeiculosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMnuVeiculosActionPerformed
+        JDlgVeiculos jDlgVeiculos = new JDlgVeiculos(null, true);
+        jDlgVeiculos.setVisible(true);// TODO add your handling code here:
+    }//GEN-LAST:event_jMnuVeiculosActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(JFrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(JFrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(JFrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(JFrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JFrmPrincipal().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuBar jMenuBar2;
+    private javax.swing.JMenu jMnuCadastro;
+    private javax.swing.JMenuItem jMnuClientes;
+    private javax.swing.JMenuItem jMnuMecanico;
+    private javax.swing.JMenu jMnuMovimento;
+    private javax.swing.JMenuItem jMnuSair;
+    private javax.swing.JMenuItem jMnuUsuarios;
+    private javax.swing.JMenuItem jMnuVeiculos;
+    private javax.swing.JMenuItem jMnuVendedor;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
+    // End of variables declaration//GEN-END:variables
+}
